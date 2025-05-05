a9efc0c3c3f23f505700a034f3e5ee985fb232e7
miadamczyk <milosz.adamczyk2002@gmail.com>
2025-05-05 07:55:32 +0200

    Separated fingerprint generation form clustering algorithms..

--- clustering_algorithms/kmeans.py
+++ clustering_algorithms/kmeans.py
@@ -1,11 +1,9 @@
 from sklearn.base import BaseEstimator, ClusterMixin
 from sklearn.cluster import KMeans
 from rdkit import Chem
-from rdkit.Chem.AllChem import GetMorganGenerator
+from utils.loader import FingerprintLoader
 
 import numpy as np
-import pandas as pd
-
 
 class KMeansFingerprintClustering(BaseEstimator, ClusterMixin):
     def __init__(self, n_clusters=3, n_bits=2048, random_state=None):
@@ -15,41 +13,23 @@ class KMeansFingerprintClustering(BaseEstimator, ClusterMixin):
         self.kmeans_model = None
         self.labels_ = None
         self.cluster_centers_ = None
+        self.loader = FingerprintLoader(n_bits)
 
-        self.fp_generator = GetMorganGenerator(radius=2, fpSize=self.n_bits)
-
-    def _load_and_transform(self, csv_path):
-        df = pd.read_csv(csv_path)
-        df = df.dropna(subset=['Smiles', 'Standard Value'])
-        smiles_list = df['Smiles'].tolist()
-        values = df['Standard Value'].astype(float).to_numpy().reshape(-1, 1)
-
-        fps = []
-        for sm in smiles_list:
-            mol = Chem.MolFromSmiles(sm)
-            bitvect = np.zeros((self.n_bits,), dtype=int)
-            if mol is not None:
-                fp = self.fp_generator.GetFingerprint(mol)
-                for i in range(self.n_bits):
-                    if fp.GetBit(i):
-                        bitvect[i] = 1
-            fps.append(bitvect)
-        fps = np.array(fps)
-
-        X = np.hstack([fps, values])
-        return X
-
-    def fit(self, csv_path):
-        X = self._load_and_transform(csv_path)
+    def fit(self, X, y=None):
+        features = self.loader.load_and_transform(X)
         self.kmeans_model = KMeans(n_clusters=self.n_clusters, random_state=self.random_state)
-        self.kmeans_model.fit(X)
+        self.kmeans_model.fit(features)
         self.labels_ = self.kmeans_model.labels_
         self.cluster_centers_ = self.kmeans_model.cluster_centers_
         return self
 
+    def fit_predict(self, X, y=None):
+        self.fit(X)
+        return self.labels_
+
     def predict(self, X):
         if isinstance(X, str):
-            X_feat = self._load_and_transform(X)
+            X_feat = self.loader.load_and_transform(X)
             return self.kmeans_model.predict(X_feat)
         else:
             if isinstance(X, list):
@@ -58,7 +38,7 @@ class KMeansFingerprintClustering(BaseEstimator, ClusterMixin):
                     mol = Chem.MolFromSmiles(sm)
                     bitvect = np.zeros((self.n_bits,), dtype=int)
                     if mol is not None:
-                        fp = self.fp_generator.GetFingerprint(mol)
+                        fp = self.loader.fp_generator.GetFingerprint(mol)
                         for i in range(self.n_bits):
                             if fp.GetBit(i):
                                 bitvect[i] = 1
@@ -69,7 +49,3 @@ class KMeansFingerprintClustering(BaseEstimator, ClusterMixin):
                 return self.kmeans_model.predict(X_feat)
             else:
                 return self.kmeans_model.predict(X)
-
-    def fit_predict(self, csv_path):
-        self.fit(csv_path)
-        return self.labels_
---
+++ utils/loader.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import numpy as np
+from rdkit import Chem
+from rdkit.Chem.AllChem import GetMorganGenerator
+
+
+class FingerprintLoader:
+    def __init__(self, n_bits=2048):
+        self.n_bits = n_bits
+        self.fp_generator = GetMorganGenerator(radius=2, fpSize=n_bits)
+
+    def load_fingerprints(self, csv_path):
+        df = pd.read_csv(csv_path)
+        df = df.dropna(subset=['Smiles'])
+        smiles_list = df['Smiles'].tolist()
+        fps = []
+        for sm in smiles_list:
+            mol = Chem.MolFromSmiles(sm)
+            fp = self.fp_generator.GetFingerprint(mol) if mol is not None else None
+            fps.append(fp)
+        return fps
+
+    def load_and_transform(self, csv_path):
+        df = pd.read_csv(csv_path)
+        df = df.dropna(subset=['Smiles', 'Standard Value'])
+        smiles_list = df['Smiles'].tolist()
+        values = df['Standard Value'].astype(float).to_numpy().reshape(-1, 1)
+
+        fps = []
+        for sm in smiles_list:
+            mol = Chem.MolFromSmiles(sm)
+            bitvect = np.zeros((self.n_bits,), dtype=int)
+            if mol is not None:
+                fp = self.fp_generator.GetFingerprint(mol)
+                for i in range(self.n_bits):
+                    if fp.GetBit(i):
+                        bitvect[i] = 1
+            fps.append(bitvect)
+        fps = np.array(fps)
+        X = np.hstack([fps, values])
+        return X
