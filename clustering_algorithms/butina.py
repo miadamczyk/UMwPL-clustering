c9ab1f2846f966af6b1e9701160d1b28c9787adf
miadamczyk <milosz.adamczyk2002@gmail.com>
2025-06-04 10:10:59 +0200

    Changes to butina algorithm

--- clustering_algorithms/butina.py
+++ clustering_algorithms/butina.py
@@ -1,51 +1,68 @@
 from sklearn.base import BaseEstimator, ClusterMixin
-from rdkit.DataStructs.cDataStructs import ExplicitBitVect, BulkTanimotoSimilarity
-from rdkit.ML.Cluster import Butina
+from rdkit.DataStructs.cDataStructs import ExplicitBitVect, TanimotoSimilarity
+from rdkit.SimDivFilters import rdSimDivPickers
 import numpy as np
 
 
 class ButinaFingerprintClustering(BaseEstimator, ClusterMixin):
     def __init__(self, similarity_threshold=0.85):
-        self.valid_indices_ = None
         self.similarity_threshold = similarity_threshold
         self.labels_ = None
         self.clusters_ = None
+        self.leader_indices_ = None
+        self.X_ = None
 
     def _validate_inputs(self, X):
         if not isinstance(X, (list, tuple)):
             raise TypeError("Input X must be a list or tuple of fingerprints.")
         for i, fp in enumerate(X):
-            if fp is not None and not isinstance(fp, ExplicitBitVect):
+            if fp is None:
+                raise ValueError(f"Element at index {i} is None.")
+            if not isinstance(fp, ExplicitBitVect):
                 raise TypeError(f"Element {i} is not an RDKit ExplicitBitVect fingerprint.")
 
     def fit(self, X, y=None):
         self._validate_inputs(X)
-        fps_valid = [fp for fp in X if fp is not None]
-        self.valid_indices_ = [i for i, fp in enumerate(X) if fp is not None]
 
-        if len(fps_valid) == 0:
-            raise ValueError("No valid fingerprints provided.")
-
-        dists = []
-        nfps = len(fps_valid)
-        for i in range(1, nfps):
-            sims = BulkTanimotoSimilarity(fps_valid[i], fps_valid[:i])
-            dists.extend([1.0 - sim for sim in sims])
-
-        clusters = Butina.ClusterData(dists, nfps, self.similarity_threshold, isDistData=True)
-        self.clusters_ = clusters
+        lp = rdSimDivPickers.LeaderPicker()
+        leader_indices = lp.LazyBitVectorPick(X, len(X), 1.0 - self.similarity_threshold)
 
         labels = [-1] * len(X)
-        for cluster_id, cluster in enumerate(clusters):
-            for local_idx in cluster:
-                global_idx = self.valid_indices_[local_idx]
-                labels[global_idx] = cluster_id
+        clusters = [[] for _ in leader_indices]
+
+        for i, fp in enumerate(X):
+            max_sim = -1
+            best_cluster = -1
+            for cluster_id, leader_idx in enumerate(leader_indices):
+                sim = TanimotoSimilarity(fp, X[leader_idx])
+                if sim > max_sim:
+                    max_sim = sim
+                    best_cluster = cluster_id
+            labels[i] = best_cluster
+            clusters[best_cluster].append(i)
 
         self.labels_ = np.array(labels)
+        self.clusters_ = clusters
+        self.leader_indices_ = leader_indices
+        self.X_ = X
         return self
 
     def predict(self, X):
-        raise NotImplementedError("Butina clustering does not support predicting new data.")
+        self._validate_inputs(X)
+        if self.leader_indices_ is None or self.X_ is None:
+            raise ValueError("Model has not been fitted yet.")
+
+        preds = []
+        for fp in X:
+            max_sim = -1
+            best_cluster = -1
+            for cluster_id, leader_idx in enumerate(self.leader_indices_):
+                sim = TanimotoSimilarity(fp, self.X_[leader_idx])
+                if sim > max_sim:
+                    max_sim = sim
+                    best_cluster = cluster_id
+            preds.append(best_cluster)
+        return np.array(preds)
 
     def fit_predict(self, X, y=None):
-        return self.fit(X, y).labels_
+        return self.fit(X, y).predict(X)
